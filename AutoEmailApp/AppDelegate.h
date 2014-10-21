99a04d3aece9dd85244af407e5ec5b9b8a14faa1
xiaominfc <793101503@qq.com>
2014-10-21 20:24:41 +0800

    Initial Commit

---
+++ AutoEmailApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AutoEmailApp
+//
+//  Created by xiaominfc on 10/21/14.
+//  Copyright (c) 2014 haitou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AutoEmailApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AutoEmailApp
+//
+//  Created by xiaominfc on 10/21/14.
+//  Copyright (c) 2014 haitou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
